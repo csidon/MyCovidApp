c890c215ec2f50b90dce9d9dd648ff36fb240247
Gumption-and-Gusto <jed.davies@gmail.com>
2022-06-20 18:52:12 +1200

    adminQRRequest progress

--- 0806_mCR_login/adminqrrequests.h
+++ 0806_mCR_login/adminqrrequests.h
@@ -1,19 +1,38 @@
 #ifndef ADMINQRREQUESTS_H
 #define ADMINQRREQUESTS_H
 
+#include "useraccount.h"
 #include <QWidget>
+#include <QObject>
 
 namespace Ui {
 class adminQRRequests;
 }
 
-class adminQRRequests : public QWidget
+class AdminQRRequests : public QWidget
 {
     Q_OBJECT
-
+private:
+    int pageNumber = 0;
 public:
-    explicit adminQRRequests(QWidget *parent = nullptr);
-    ~adminQRRequests();
+    explicit AdminQRRequests(QWidget *parent = nullptr);
+    ~AdminQRRequests();
+    void setDisplayedUsers(int newPageNumber);
+
+    int getPageNumber();
+    void setPageNumber(int newPageNumber);
+    void updatePageNumberDisplay();
+    QString formatNameForDisplay(UserAccount dataSource);
+signals:
+    void pageTurned(int);
+private slots:
+    void on_btn_pageRight_clicked();
+
+    void on_btn_pageLeft_clicked();
+
+    void on_btn_backToAdminHome_clicked();
+
+    void setDisplay(int topUser);
 
 private:
     Ui::adminQRRequests *ui;
--- build-0806_mCR_login-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/0806_mCR_login_autogen/EWIEGA46WW/moc_adminqrrequests.cpp
+++ build-0806_mCR_login-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/0806_mCR_login_autogen/EWIEGA46WW/moc_adminqrrequests.cpp
@@ -21,54 +21,100 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_adminQRRequests_t {
-    const uint offsetsAndSize[2];
-    char stringdata0[16];
+struct qt_meta_stringdata_AdminQRRequests_t {
+    const uint offsetsAndSize[16];
+    char stringdata0[127];
 };
 #define QT_MOC_LITERAL(ofs, len) \
-    uint(offsetof(qt_meta_stringdata_adminQRRequests_t, stringdata0) + ofs), len 
-static const qt_meta_stringdata_adminQRRequests_t qt_meta_stringdata_adminQRRequests = {
+    uint(offsetof(qt_meta_stringdata_AdminQRRequests_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_AdminQRRequests_t qt_meta_stringdata_AdminQRRequests = {
     {
-QT_MOC_LITERAL(0, 15) // "adminQRRequests"
+QT_MOC_LITERAL(0, 15), // "AdminQRRequests"
+QT_MOC_LITERAL(16, 10), // "pageTurned"
+QT_MOC_LITERAL(27, 0), // ""
+QT_MOC_LITERAL(28, 24), // "on_btn_pageRight_clicked"
+QT_MOC_LITERAL(53, 23), // "on_btn_pageLeft_clicked"
+QT_MOC_LITERAL(77, 30), // "on_btn_backToAdminHome_clicked"
+QT_MOC_LITERAL(108, 10), // "setDisplay"
+QT_MOC_LITERAL(119, 7) // "topUser"
 
     },
-    "adminQRRequests"
+    "AdminQRRequests\0pageTurned\0\0"
+    "on_btn_pageRight_clicked\0"
+    "on_btn_pageLeft_clicked\0"
+    "on_btn_backToAdminHome_clicked\0"
+    "setDisplay\0topUser"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_adminQRRequests[] = {
+static const uint qt_meta_data_AdminQRRequests[] = {
 
  // content:
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   44,    2, 0x06,    1 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       3,    0,   47,    2, 0x08,    3 /* Private */,
+       4,    0,   48,    2, 0x08,    4 /* Private */,
+       5,    0,   49,    2, 0x08,    5 /* Private */,
+       6,    1,   50,    2, 0x08,    6 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
 
        0        // eod
 };
 
-void adminQRRequests::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void AdminQRRequests::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
-    (void)_a;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<AdminQRRequests *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->pageTurned((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 1: _t->on_btn_pageRight_clicked(); break;
+        case 2: _t->on_btn_pageLeft_clicked(); break;
+        case 3: _t->on_btn_backToAdminHome_clicked(); break;
+        case 4: _t->setDisplay((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (AdminQRRequests::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AdminQRRequests::pageTurned)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
 }
 
-const QMetaObject adminQRRequests::staticMetaObject = { {
+const QMetaObject AdminQRRequests::staticMetaObject = { {
     QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
-    qt_meta_stringdata_adminQRRequests.offsetsAndSize,
-    qt_meta_data_adminQRRequests,
+    qt_meta_stringdata_AdminQRRequests.offsetsAndSize,
+    qt_meta_data_AdminQRRequests,
     qt_static_metacall,
     nullptr,
-qt_incomplete_metaTypeArray<qt_meta_stringdata_adminQRRequests_t
-, QtPrivate::TypeAndForceComplete<adminQRRequests, std::true_type>
-
+qt_incomplete_metaTypeArray<qt_meta_stringdata_AdminQRRequests_t
+, QtPrivate::TypeAndForceComplete<AdminQRRequests, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>
 
 
 >,
@@ -76,23 +122,41 @@ qt_incomplete_metaTypeArray<qt_meta_stringdata_adminQRRequests_t
 } };
 
 
-const QMetaObject *adminQRRequests::metaObject() const
+const QMetaObject *AdminQRRequests::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *adminQRRequests::qt_metacast(const char *_clname)
+void *AdminQRRequests::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_adminQRRequests.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_AdminQRRequests.stringdata0))
         return static_cast<void*>(this);
     return QWidget::qt_metacast(_clname);
 }
 
-int adminQRRequests::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int AdminQRRequests::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 5;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void AdminQRRequests::pageTurned(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
