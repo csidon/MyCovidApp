b4aef328357ebb04f2f7ffe1b5edaa5b9b9920fd
cSidon <csidon@protonmail.com>
2022-06-22 15:17:34 +1200

    Adding RAT Test done. Needs testing. Writing to CSV file now works. All csv files now read and write to AppData folder!

--- 0806_mCR_login/handlecsv.cpp
+++ 0806_mCR_login/handlecsv.cpp
@@ -1,5 +1,8 @@
 #include "handlecsv.h"
 #include "useraccount.h"
+#include <QCoreApplication>
+#include <QStandardPaths>
+
 HandleCSV::HandleCSV()
 {
 
@@ -13,16 +16,28 @@ QString HandleCSV::returnCSVFilePath(QString dbName)
     qDebug() << "dbName passed is " << dbName;
     if (dbName == "dbPID")
     {
-        filePath = ":/database/dummyPID.csv";
+        filePath = "./database/dummyPID.csv";
 //        qDebug() << "You have found dbPID with filepath " << filePath;
+    }
+    else if (dbName == "readPID")
+    {
+
     }
     else if (dbName == "dbTest")
     {
-        filePath = ":/database/MasterTests.csv";
+        filePath = "database/MasterTests.csv";
     }
     else if (dbName == "dbDose")
     {
-        filePath = ":/database/MasterDoses.csv";
+        filePath = "./database/MasterDoses.csv";
+    }
+    else if (dbName == "dbQRRequests")
+    {
+        filePath = "database/QRCodeRequests.csv";
+    }
+    else if (dbName == "dbReports")
+    {
+        filePath = "database/ErrorReports.csv";
     }
     // ** Add any other database/path here
     else
@@ -40,33 +55,67 @@ int HandleCSV::returnHeaderIndex(QString dbName, QString headerName)
 {
     int headerIndex = -1;
 
-    QString filePath = returnCSVFilePath(dbName);
-    qDebug() << "File path is " <<  filePath;
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+    QDir d{path};
+    QString filepath = returnCSVFilePath(dbName);
 
-    // Open CSV filepath retrieved from associated dbName
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
     {
-        qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
-                 << "Filepath for getUserAccount = " << filePath;
-        return headerIndex;
-    }
-
-    // Read the header line and get the value of each cell
-    QString headers = file.readLine().trimmed();
-    QStringList headerRowValues = headers.split(',');
-
-    // Compare that cell value to the colHeader search value
-    // and grab the column index that we are retrieving
-    for(int i = 0; i < headerRowValues.size(); i++)
-    {
-        qDebug() << "Comparing colHeaders...";
-        if(headerRowValues.at(i) == headerName)
+        qDebug() << "Reading from " << QDir::currentPath();
+        QFile f{filepath};
+        if (f.open(QIODevice::ReadOnly | QIODevice::Text))
         {
-            return i;
+            // Read the header line and get the value of each cell
+            QString headers = f.readLine().trimmed();
+            QStringList headerRowValues = headers.split(',');
+
+            // Compare that cell value to the colHeader search value
+            // and grab the column index that we are retrieving
+            for(int i = 0; i < headerRowValues.size(); i++)
+            {
+//                qDebug() << "Comparing colHeaders...";
+//                qDebug() << "Value of i " << i;
+//                qDebug() << "What is the headerRowValue size? " << headerRowValues.size();
+//                qDebug() << "HeaderRowVal at i: " << headerRowValues.at(i)
+//                         << " and header name is " << headerName;
+                if(headerRowValues.at(i) == headerName)
+                {
+                    return i;
+                }
+            }
         }
     }
     return headerIndex;
+//    int headerIndex = -1;
+
+//    QString filePath = returnCSVFilePath(dbName);
+//    qDebug() << "File path is " <<  filePath;
+
+//    // Open CSV filepath retrieved from associated dbName
+//    QFile file(filePath);
+//    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+//    {
+//        qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
+//                 << "Filepath for getUserAccount = " << filePath;
+//        return headerIndex;
+//    }
+
+//    // Read the header line and get the value of each cell
+//    QString headers = file.readLine().trimmed();
+//    QStringList headerRowValues = headers.split(',');
+
+//    // Compare that cell value to the colHeader search value
+//    // and grab the column index that we are retrieving
+//    for(int i = 0; i < headerRowValues.size(); i++)
+//    {
+//        qDebug() << "Comparing colHeaders...";
+//        if(headerRowValues.at(i) == headerName)
+//        {
+//            return i;
+//        }
+//    }
+//    return headerIndex;
 }
 
 //-----------------------------------------------------------
@@ -74,30 +123,34 @@ int HandleCSV::returnHeaderIndex(QString dbName, QString headerName)
 QStringList HandleCSV::getColData(QString headerName, QString dbName)
 {
     QStringList data;
-    qDebug() << "getColData before the returnCSVfilepath";
-    QString filePath = returnCSVFilePath(dbName);
-    qDebug() << "You are in getColData and your filepath is " << filePath;
-
     int headerIndex = returnHeaderIndex(dbName,headerName);
 
-    // Open CSV filepath retrieved from associated dbName
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
-                 << "Filepath for getUserAccount = " << filePath;
-        return data;
-    }
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+    QDir d{path};
+    QString filePath = returnCSVFilePath(dbName);
 
-    // Retrieve all of the values in that headerIndex's column
-    while(!file.atEnd())
+    if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
     {
-        // Read the line and store it
-        QString row = file.readLine().trimmed();
-        QStringList rowValues = row.split(',');
-        data << rowValues.at(headerIndex);
+        qDebug() << "Reading from " << QDir::currentPath();
+        QFile file{filePath};
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
+                     << "Filepath for getUserAccount = " << filePath;
+            return data;
+        }
+
+        // Retrieve all of the values in that headerIndex's column
+        while(!file.atEnd())
+        {
+            // Read the line and store it
+            QString row = file.readLine().trimmed();
+            QStringList rowValues = row.split(',');
+            data << rowValues.at(headerIndex);
+        }
+        file.close();
     }
-    file.close();
     return data;    // Returns all the data in that column as a QStringList
 }
 
@@ -124,41 +177,43 @@ int HandleCSV::rowIndexOfCellMatchingSearch(QStringList colData, QString searchV
 QString HandleCSV::getCellValue(QString dbName, int headerIn, int rowIn)
 {
     QString cellValue;
+
+
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+    QDir d{path};
     QString filePath = returnCSVFilePath(dbName);
-    qDebug() << "Filepath for getCellVal = "<< filePath;
 
-    // Open CSV filepath retrieved from associated dbName
-    QFile file(filePath);
-
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
     {
-        qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
-                 << "Filepath for getUserAccount = " << filePath;
-        return cellValue;
-    }
-
-    // Retrieve all of the values in that headerIndex's column
-    while(!file.atEnd())
-    {
-
-        // Skip the number of rows that are inapplicable
-        int rowNum = 1;
-        do
+        qDebug() << "Reading cells from " << QDir::currentPath();
+        QFile file{filePath};
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         {
-            file.readLine();
-            rowNum++;
-        } while(rowNum < rowIn);
+            qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
+                     << "Filepath for getUserAccount = " << filePath;
+            return cellValue;
+        }
+        // Retrieve all of the values in that headerIndex's column
+        while(!file.atEnd())
+        {
+            // Skip the number of rows that are inapplicable
+            for (int i = 0; i < rowIn; i++)
+            {
+                file.readLine();
+            }
 
-        QString row = file.readLine().trimmed();
-        QStringList rowValues = row.split(',');
-        cellValue = rowValues.at(headerIn);
-//        qDebug() << "You made it here!";
-        file.close();
-        return cellValue;
+            qDebug() << "RowIndex is: " << rowIn;
+            QString row = file.readLine().trimmed();
+            qDebug() << "The row val is " << row;
+            QStringList rowValues = row.split(',');
+            cellValue = rowValues.at(headerIn);
+    //        qDebug() << "You made it here!";
+            file.close();
+            return cellValue;
+        }
     }
-
     return cellValue;
-
 }
 
 UserAccount HandleCSV::getUserAccount(QString email)
@@ -179,36 +234,43 @@ UserAccount HandleCSV::getUserAccount(QString email)
     else
     {
         //Get all the data from that user based on row index
-
+        auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+        QDir d{path};
         QString filePath = returnCSVFilePath("dbPID");
 
-        // Open CSV filepath retrieved from associated dbName
-        QFile file(filePath);
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
         {
-            qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
-                     << "Filepath for getUserAccount = " << filePath;
+            qDebug() << "Reading from " << QDir::currentPath();
+            QFile file{filePath};
+
+            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            {
+                qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
+                         << "Filepath for getUserAccount = " << filePath;
+                return grabbedUser;
+            }
+
+            // Skip rows that are not applicable
+            int rowNum = 1;
+            do
+            {
+                file.readLine();
+                rowNum++;
+            } while(rowNum < userFoundInRowIndex);
+
+            QString row = file.readLine().trimmed();
+            QStringList rowValues = row.split(',');
+
+            grabbedUser = UserAccount(rowValues.at(0).toInt(),rowValues.at(1),rowValues.at(2),
+                                      rowValues.at(3),rowValues.at(4),rowValues.at(5),
+                                      rowValues.at(6),rowValues.at(7).toInt(),
+                                      rowValues.at(8).toInt(),rowValues.at(9).toInt(),rowValues.at(10));
+            file.close();
             return grabbedUser;
         }
-
-        // Skip rows that are not applicable
-        int rowNum = 1;
-        do
-        {
-            file.readLine();
-            rowNum++;
-        } while(rowNum < userFoundInRowIndex);
-
-        QString row = file.readLine().trimmed();
-        QStringList rowValues = row.split(',');
-
-        grabbedUser = UserAccount(rowValues.at(0).toInt(),rowValues.at(1),rowValues.at(2),
-                                  rowValues.at(3),rowValues.at(4),rowValues.at(5),
-                                  rowValues.at(6),rowValues.at(7).toInt(),
-                                  rowValues.at(8).toInt(),rowValues.at(9).toInt(),rowValues.at(10));
-        file.close();
-        return grabbedUser;
     }
+    return grabbedUser;
 
 }
 
@@ -230,82 +292,85 @@ UserAccount HandleCSV::getUserAccount(int uid)
     else
     {
         //Get all the data from that user based on row index
+        auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+        QDir d{path};
         QString filePath = returnCSVFilePath("dbPID");
 
-        // Open CSV filepath retrieved from associated dbName
-        QFile file(filePath);
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
         {
-            qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
-                     << "Filepath for getUserAccount = " << filePath;
+            qDebug() << "Reading from " << QDir::currentPath();
+            QFile file{filePath};
+            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            {
+                qDebug() << "IsOpen?: "<< file.isOpen()  << file.errorString()
+                         << "Filepath for getUserAccount = " << filePath;
+            }
+
+            // Skip rows that are not applicable
+            int rowNum = 1;
+            do
+            {
+                file.readLine();
+                rowNum++;
+            } while(rowNum < userFoundInRowIndex);
+
+            QString row = file.readLine().trimmed();
+            QStringList rowValues = row.split(',');
+
+            grabbedUser = UserAccount(rowValues.at(0).toInt(),rowValues.at(1),rowValues.at(2),
+                                      rowValues.at(3),rowValues.at(4),rowValues.at(5),
+                                      rowValues.at(6),rowValues.at(7).toInt(),
+                                      rowValues.at(8).toInt(),rowValues.at(9).toInt(),rowValues.at(10));
+            file.close();
+
         }
-
-        // Skip rows that are not applicable
-        int rowNum = 1;
-        do
-        {
-            file.readLine();
-            rowNum++;
-        } while(rowNum < userFoundInRowIndex);
-
-        QString row = file.readLine().trimmed();
-        QStringList rowValues = row.split(',');
-
-        grabbedUser = UserAccount(rowValues.at(0).toInt(),rowValues.at(1),rowValues.at(2),
-                                  rowValues.at(3),rowValues.at(4),rowValues.at(5),
-                                  rowValues.at(6),rowValues.at(7).toInt(),
-                                  rowValues.at(8).toInt(),rowValues.at(9).toInt(),rowValues.at(10));
-        file.close();
-
     }
     return grabbedUser;
-
 }
 
-void HandleCSV::writeToPIDCSV(UserAccount userConstructor)
+void HandleCSV::writeToPIDCSV(UserAccount newUser)
 {
-    // Storing in userPID db
-    QString filePath = returnCSVFilePath("dbPID");
-    qDebug() << "File path passsed to writeToPIDCSV is " <<  filePath;
-
     // Mapping info in userConstructor to cells
     QString email, pass, fn, ln, pn, nhi, qrAdd;
     int uid, ph, vaxstat,qrstat;
-    uid = userConstructor.getUserIDNumber();
-    email = userConstructor.getUserEmail();
-    pass = userConstructor.getUserPassword();
-    fn = userConstructor.getUserFirstName();
-    ln = userConstructor.getUserLastName();
-    pn = userConstructor.getUserPreferredName();
-    nhi = userConstructor.getUserNHINumber();
-    qrAdd = userConstructor.getUserQRCodeAddress();
-    ph = userConstructor.getUserPhoneNumber();
-    vaxstat = userConstructor.getUserVaccinationStatus();
-    qrstat = userConstructor.getUserQRStatus();
+    uid = newUser.getUserIDNumber();
+    email = newUser.getUserEmail();
+    pass = newUser.getUserPassword();
+    fn = newUser.getUserFirstName();
+    ln = newUser.getUserLastName();
+    pn = newUser.getUserPreferredName();
+    nhi = newUser.getUserNHINumber();
+    qrAdd = newUser.getUserQRCodeAddress();
+    ph = newUser.getUserPhoneNumber();
+    vaxstat = newUser.getUserVaccinationStatus();
+    qrstat = newUser.getUserQRStatus();
 
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+    QDir d{path};
+    QString filepath = returnCSVFilePath("dbPID");
 
-    // Open CSV filepath retrieved from associated dbName
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadWrite | QIODevice::Append))
+    if (d.mkpath(d.absolutePath()) && QDir::setCurrent(d.absolutePath()))
     {
-
-        qDebug() << file.isOpen() << "error " << file.errorString();
-
+        qDebug() << "Writing to " << QDir::currentPath();
+        QFile f{filepath};
+        qDebug() << "Filepath is " << filepath;
+        if (!f.open(QIODevice::ReadWrite | QIODevice::Append))
+        {
+            qDebug() << "Is the file open? " << f.isOpen();
+            qDebug() << "File error: " << f.error();
+            qDebug() << "Error string: " << f.errorString();
+        }
+        else
+        {
+            QTextStream stream(&f);
+            stream << uid << "," << email << "," << pass << "," << fn << ","
+                   << ln << "," << pn << "," << nhi << "," << ph << ","
+                   << vaxstat << "," << qrstat << "," << qrAdd << "\n";
+            qDebug() << "I have theoretically streamed data.";
+        }
+//        f.close();
     }
-//    if (file.open(QIODevice::ReadWrite | QIODevice::Append))
 
-    else
-    {
-        qDebug() << "Is the file open?" << file.isOpen();
-
-        // Streaming info back into db
-        QTextStream stream(&file);
-        stream << uid << "," << email << "," << pass << "," << fn << ","
-               << ln << "," << pn << "," << nhi << "," << ph << ","
-               << vaxstat << "," << qrstat << "," << qrAdd << "\n";
-
-    }
-
-
-    file.close();
 }
